e0c1aafc9749525d6e664e74d7539a101305300a
David Thigpen <davidthigs@gmail.com>
2018-09-29 19:14:52 -0500

    initial file state

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ This program will creates a new file from an existing file.  The old file could 
 4. Link the new file  inode in the root directory inode.
 5. Do ls in the root directory.  The file name you created should show in ls command.
 6. Open the file to make sure everything is same as in old file. 
+
+### Note
+This project cannot currently compile and run on its own. it requires the utd digital-forensics repository for its dependecies
---
+++ copyfilemain.c
@@ -0,0 +1,109 @@
+/*******************************************************************
+ *
+ * File         : copyfilemain.c
+ * Author       : 
+ * Date created : September 29, 2018
+ * Description  : This program copies a file to the root directory by
+ *                allocating a new inode and copying over all
+ *                information from the old inode and adding a new
+ *                entry to the root directory for the file.
+ *
+ *******************************************************************/
+#include "inodeinc.h"
+#include "inodecli.h"
+
+#include <sys/stat.h>
+
+
+
+INT4 inode_from_filepath(int fd);
+
+/*! *************************************************************************
+ * Function Name : main 
+ *
+ * Description   : This is the main() function for the inode program.
+ *
+ * Input         : argc - total no. of command line arguments
+ *                 argv - command line arguments
+ *
+ * Output        : None
+ * 
+ * Returns       : None
+ *
+ **************************************************************************/
+VOID main(INT4 argc, CHAR** argv )
+{
+    CHAR *DEFAULT_DEV = "/dev/sdb1";
+    CHAR *pDev;
+    CHAR *filepath;
+    INT4 fd, old_inode, new_inode;
+
+
+    if (argc == 1)
+    {
+        printf("Enter disk name as command line argument. e.g - sudo ./copyfile /dev/sda1 filepath\n");
+        //return -1;
+    }else if(argc == 2){
+        printf("Using fefault device: %s", DEFAULT_DEV);
+        pDev = DEFAULT_DEV;
+        filepath = argv[2];
+    }else{
+        pDev = argv[1];
+        filepath = argv[2];
+        
+        if(argc > 3)
+        {
+            printf("Only using the first two args.");
+        }
+    }
+    
+    printf("Args: dev: %s filename: %s",pDev,filepath);
+    fd = open(filepath, O_RDONLY);
+    if(fd < 0){
+        perror("Error opening file");
+        return -1;
+    }
+
+    old_inode = inode_from_filepath(fd);
+    printf("Inode: %d\n", old_inode);
+
+    // InodeInit(pDev);
+    // printf("Total inodes: %d, Inodes per group: %d, Size of inode: %d\n",
+    //         sb.s_inodes_count,
+    //         sb.s_inodes_per_group,
+    //         sb.s_inode_size);
+    // printf("Block Size: %u Bytes \n", gu4BlockSize);
+
+    // InodeCliStart();
+    // InodeInitExit();
+}
+
+/*! *************************************************************************
+ * Function Name :  inode_from_filepath
+ *
+ * Description   : This function returns the inode of a particular file.
+ *
+ * Input         : fd - the file descriptor of the file to get the inode for.
+ *
+ * Output        : None
+ * 
+ * Returns       : INT4 - the inode number of the file that was input
+ *
+ **************************************************************************/
+INT4 inode_from_filepath(int fd)
+{
+    struct stat file_stat;
+    INT4 ret;
+    ret = fstat(fd, &file_stat);
+    if(ret < 0){
+        perror("Error getting file stats");
+    }
+    return file_stat.st_ino;
+    
+}
+
+
+
+
+
+
